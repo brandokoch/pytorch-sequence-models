ba61e79066b220bd2f34d787d89bad7c87d4004e
Brando Koch <57716666+bkoch4142@users.noreply.github.com>
2021-06-25 12:34:13 +0200

    Update sentiment_clf.py

--- src/models/sentiment_clf.py
+++ src/models/sentiment_clf.py
@@ -36,30 +36,3 @@ class SentimentClassifier(nn.Module):
         # We dont apply sigmoid to output since nn.BCEWithLogitsLoss
         # combines a Sigmoid layer and the BCELoss
         return torch.squeeze(out, dim=1)
-
-
-class SentimentClassifier1(nn.Module):
-    def __init__(self, vocab_sz, n_hidden):
-        super(SentimentClassifier1, self).__init__()
-
-        self.embedding = nn.Embedding(vocab_sz, n_hidden)
-
-        self.rnn = nn.LSTM(n_hidden, n_hidden, batch_first=True)
-
-        self.linear = nn.Linear(n_hidden*2, 1)
-
-    def forward(self, x):
-        x = self.embedding(x)
-        x, _ = self.rnn(x)
-
-        # Using the avg and max pool of all RNN outputs
-        avg_pool = torch.mean(x, dim=1)
-        max_pool, _ = torch.max(x, 1)
-
-        # We concatenate them (hidden size before the linear layer is multiplied by 2)
-        out = torch.cat((avg_pool, max_pool), dim=1)
-        out = self.linear(out)
-
-        # We dont apply sigmoid to output since nn.BCEWithLogitsLoss
-        # combines a Sigmoid layer and the BCELoss
-        return torch.squeeze(out, dim=1)
